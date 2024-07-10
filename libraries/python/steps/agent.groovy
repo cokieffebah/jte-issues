e723d61fa2fef7a6eb6aa2b327d3f01b707f9139
Carlos OKieffe <o'kieffe_carlos@bah.com>
2024-07-10 15:25:06 -0400

    Update agent.groovy
    removed containerTemplate

--- libraries/python/steps/agent.groovy
+++ libraries/python/steps/agent.groovy
@@ -4,7 +4,7 @@ void call(Map args = [:], body) {
   def version = config.version ?: '3'
 
   podTemplate(
-    containers: [containerTemplate(name: 'python', image: "python:${version}", command: 'sleep', args: '1d')]
+    containers: [name: 'python', image: "python:${version}", command: 'sleep', args: '1d']
   ) {
     node(POD_LABEL) {
       container('python') {
