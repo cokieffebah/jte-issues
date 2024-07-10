4828f6e7f29987427fab74558f21e451cb485b4c
Carlos OKieffe <o'kieffe_carlos@bah.com>
2024-07-10 15:32:06 -0400

    Update agent.groovy
    removed kube parts

--- libraries/python/steps/agent.groovy
+++ libraries/python/steps/agent.groovy
@@ -3,13 +3,11 @@ package library.python.steps
 void call(Map args = [:], body) {
   def version = config.version ?: '3'
 
-  podTemplate(
-    containers: [name: 'python', image: "python:${version}", command: 'sleep', args: '1d']
-  ) {
+  
     node(POD_LABEL) {
       container('python') {
         body()
       }
     }
-  }
+
 }
